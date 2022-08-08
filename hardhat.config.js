bf419679108eaa2d21ac6f5bf40501eca24fae0d
Anush GS <72384920+anush443@users.noreply.github.com>
2022-08-08 19:53:32 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,10 +10,9 @@ const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 //     process.env.KOVAN_RPC_URL ||
 //     "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const RINKEBY_RPC_URL =
-    process.env.RINKEBY_RPC_URL ||
-    "https://eth-rinkeby.alchemyapi.io/v2/LnMii3GfLzYj6B20vHDtRwiJhIY1OcHz"
+    process.env.RINKEBY_RPC_URL || ""
 const PRIVATE_KEY =
-    process.env.PRIVATE_KEY || "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+    process.env.PRIVATE_KEY || ""
 //const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 module.exports = {
